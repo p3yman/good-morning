c4259ebe59176524214865a9b8e533eb7cdb3255
Peyman Eskandari <peyman.eskandari@gmail.com>
2020-05-27 07:43:10 +0300

    Updates

---
+++ precache-manifest.6194fb11ed0b955f4135994522a42cf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "723753cf456e8d3fbe775d4f576f26ea",
+    "url": "/good-morning/index.html"
+  },
+  {
+    "revision": "a6942c5d57f757016d12",
+    "url": "/good-morning/static/css/main.1489b5c3.chunk.css"
+  },
+  {
+    "revision": "e0a542e41db1c020dbd3",
+    "url": "/good-morning/static/js/2.50ddae00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/good-morning/static/js/2.50ddae00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6942c5d57f757016d12",
+    "url": "/good-morning/static/js/main.a3e99f59.chunk.js"
+  },
+  {
+    "revision": "5e84ced652413d934779",
+    "url": "/good-morning/static/js/runtime-main.582dc826.js"
+  }
+]);
